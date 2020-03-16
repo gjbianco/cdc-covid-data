f5556daa11229a572a80492dc6ac7a071cda401e
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-03-16 11:39:16 -0400

    pull state data as well

--- src/fetch.js
+++ src/fetch.js
@@ -1,28 +1,42 @@
 const axios = require('axios');
 const fs = require('fs');
+const parseCsv = require('csv-parse/lib/sync');
 
 async function fetchData() {
+  writeFile(`onset-${new Date().getTime()}`, await getOnsetData());
+  writeFile(`state-${new Date().getTime()}`, await getStateData());
+}
+
+function writeFile(filename, data) {
+  fs.writeFile(`./db/${filename}.json`, JSON.stringify(data), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(`The file ${filename} was saved!`);
+  });
+}
+
+async function getOnsetData() {
   const response = await axios.get(
     'https://www.cdc.gov/coronavirus/2019-ncov/us-cases-epi-chart.json'
   );
   const caseRawData = response.data.data.columns;
-  const transformedData = caseRawData[0].map((caseDate, index) => ({
+  return caseRawData[0].map((caseDate, index) => ({
     date: caseDate,
     value: caseRawData[1][index]
   }));
-  transformedData.unshift();
+}
 
-  const filename = new Date().getTime();
-  fs.writeFile(
-    `./db/${filename}.json`,
-    JSON.stringify(transformedData),
-    function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(`The file ${filename} was saved!`);
-    }
+async function getStateData() {
+  const response = await axios.get(
+    'https://www.cdc.gov/coronavirus/2019-ncov/map-data-cases.csv'
   );
+  const csvData = response.data;
+  const stateRawData = parseCsv(csvData, {
+    columns: true,
+    skip_empty_lines: true
+  });
+  return stateRawData; // TODO transform?
 }
 
 module.exports = fetchData;
